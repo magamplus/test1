8a7aaa79bb497150dae2d097dcc352bcbdfe7f25
magamplus <3bqjzu@30mail.ir>
2016-01-08 16:52:01 +0330

    Update version.lua

--- plugins/version.lua
+++ plugins/version.lua
@@ -2,8 +2,9 @@ do
 
 function run(msg, matches)
   return 'Telegram Bot '.. VERSION .. [[ 
-  Checkout http://git.io/6jdjGg
-  GNU GPL v2 license.]]
+  amir/amir/abas
+  version:1.1
+  باتشکر از یاگوپ
 end
 
 return {
